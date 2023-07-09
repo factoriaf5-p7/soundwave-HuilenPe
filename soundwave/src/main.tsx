153b40a08c2118b3a477ca43d5976a7e26b04371
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-07-09 22:29:41 +0200

    componens layout

--- soundwave/src/main.tsx
+++ soundwave/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/global.css';
+import { Layout } from 'antd';
+import AppHeader from './components/Header/Header';
+import AppContent from './components/body/body';
+import { Footer } from 'antd/es/layout/layout';
+import './css/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout> 
+      <AppHeader />
+      <AppContent />
+      <Footer/>
+    </Layout>  
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
